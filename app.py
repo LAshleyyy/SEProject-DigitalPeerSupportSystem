33786526e0dfecab0166f8922de661648f87717f
Ashley <al2024059@gmail.com>
2026-02-13 00:07:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,10 +45,10 @@ cloudinary.config(
 # ==========================================
 # EMAIL CONFIGURATION
 # ==========================================
-SMTP_SERVER = "smtp.gmail.com"  # Google's SMTP server
-SMTP_PORT = 587                 # TLS Port
-SENDER_EMAIL = "al2024059@gmail.com" 
-SENDER_PASSWORD = "xwla zexd aybo rois" # App Pass 
+SMTP_SERVER = os.getenv("SMTP_SERVER")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
 
 # ==========================================
 # UPDATED CONFIG & HELPERS
